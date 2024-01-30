3c06c8cfaeebb0e810c898c29456ac77af973017
JesCorAvi <jesuscorderoavila1@gmail.com>
2024-01-30 19:01:13 +0100

    migraciones y modelos "acabados"

---
+++ app/Models/Comment.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Comment extends Model
+{
+    use HasFactory;
+    public function commentable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+    public function comments(): MorphMany
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
