81aaa60d68909dd6344a575ed6647608661eabac
Pablo Galo <pablo.galo@iesdonana.org>
2024-01-31 19:11:44 +0100

    Componente-noticia en proceso

---
+++ resources/views/components/noticia.blade.php
@@ -0,0 +1,4 @@
+<div {{ $attributes->merge(['type' => 'button', 'class' =>'py-10 px-0 m-0 position: relative border-style: solid']) }}>
+    {{$prueba}} {{ $slot }}
+</div>
+
---
+++ resources/views/prueba.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+<x-noticia prueba='hola'> adios</x-noticia>
+
+<style>
+    .container{
+        border: solid;
+        size: auto;
+    }
+    .cuerpo{
+        display: inline;
+    }
+
+</style>
+<div class="container">
+    <div class="cuerpo">
+
+    </div>
+    <button>Meneos</button>
+    <a>Titulo de la noticia</a>
+    <img class="imagen" src="https://cdn.mnmstatic.net/cache/3b/8f/media_thumb-link-3903245.jpeg?1706710926">
+</div>
+</x-app-layout>
