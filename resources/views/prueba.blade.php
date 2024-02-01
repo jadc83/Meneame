43f3a3e2b478f5f95ca3dd97995ea1e617472f10
Pablo Galo <pablo.galo@iesdonana.org>
2024-02-01 20:12:29 +0100

    Noticias terminado - en espera foto,etiqueta

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -1,50 +1,7 @@
 <x-app-layout>
-    <div class="flex flex-col relative items-center mx-auto my-auto bg-white rounded-lg shadow md:flex-row md:max-w-5xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-            <div class="bg-naranjita news-shakeit text-xs text-center text-orange-500 absolute top-6 left-4 h-24 w-16">
-                <div class="votes py-1 border border-white border-b-0 leading-4 bg-repeat-x bg-position-0 rounded-t-3 font-bold">
-                    <a href=# class="leading-none ga-event text-orange-500 no-underline text-base font-bold">103 meneos</a>
-                </div>
-                <div class="overflow-visible w-18 absolute top-18">
-                    <a href=# class="px-4 overflow-hidden  bg-gradient-to-r from-orange-500 to-orange-600 shadow-md border border-orange-900 p-1 block text-white no-underline font-bold ">menéalo</a>
-                </div>
-                <div class="clics py-1 border border-white border-t-0 text-10 bg-repeat-x bg-position-bottom rounded-b-3 font-bold pt-8">
-                    <span>167</span> clics
-                </div>
-            </div>
-
-        <div class="flex flex-col justify-between p-4 pl-28 pr-40 leading-normal">
-
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Bruselas insiste en renovar el Poder Judicial antes de su reforma tras la primera cita de la mediación </h5>
-
-
-            <div class="text-xs min-h-7">
-                <a href="$user-profile" class="inline-block relative opacity-100 z-0 font-inherit cursor-pointer">
-                    <img src="https://old.meneame.net/cache/0a/60/680004-1618897561-25.jpg" class="w-6 h-6 mr-1 inline-block align-middle rounded-full">
-                </a>
-                por
-                 <a href="$perfil" class="inline-block">$article->user->name</a>
-                a
-                <span class="">$fuente</span>
-                &nbsp;
-                <span class="inline-block">hora</span>
-                publicado: <span class="inline-block">23 minutos</span>
-            </div>
-
-            <p class="mb-3">Fin de la primera reunión para la mediación de la Comisión Europea para la renovación del Consejo General del Poder Judicial. 90 minutos de toma de contacto tras cinco años de bloqueo y dos acuerdos fallidos de los que el PP se retractó en el último momento. La Comisión Europea ha reiterado sus conclusiones habituales: que se debe renovar el Poder Judicial como “prioridad” e “inmediatamente después” iniciar los trámites para cambiar el modelo. Ese marco no le gusta al PP</p>
-            <div class="h-8 text-sm border-t border-b border-orange-200">
-                <div class="mt-1">
-                    <a class="bg-naranjita text-orange-500 pointer cursor-pointer p-1 pt-1.5 pb-2 pl-2 pr-4 font-bold">17 comentarios</a>
-                    <a class=" text-orange-400 border-r border-orange-300 p-1 pl-3 pr-3">compartir</a>
-                    <span class=" font-bold p-1 pl-3 pr-2 text-xs">69</span>
-                    <span class=" font-bold p-1 pl-3 pr-2 text-xs">29</span>
-                    <span class=" font-bold border-r border-grey-300 p-1 pl-3 pr-3 text-xs">3</span>
-                    <span class="font-bold border-r border-grey-300 p-1 pl-3 pr-3 text-xs"><span class="text-orange-400">K</span>&nbsp; 449</span>
-                    <a class="p-1 pl-2 pr-2 bg-gray-400 text-white rounded-full text-xs">tecnologia</a>
-                </div>
-            </div>
-        </div>
-
-        <img src="https://cdn.mnmstatic.net/cache/3b/8f/media_thumb-link-3903245.jpeg?1706710926" class="absolute top-6 right-0" alt="">
-
-    </div>
+    @php
+    $article = App\Models\Article::find(1);
+    $fuente =$article->link
+    @endphp
+<x-noticia :article="$article"></x-noticia>
 </x-app-layout>
